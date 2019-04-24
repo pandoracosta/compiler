dbe690bb675f05703a69288abb1ed578eecde483
pandoracosta <pandorahari@hotmail.com>
2019-04-24 16:42:10 -0300

    falta 2 casos

--- lexicalAnalyser.cpp
+++ lexicalAnalyser.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,9 +11,9 @@
 #include <set> 
 using namespace std;
 
-int cont,coluna, linha, last=0, bug=0;
+int cont,coluna, linha, error=0,lixo=0, last=0, bug=0, slinha, scol;
 string buffer;
-
+bool enter = false, para= false;
 void fail() {
   cout<<"ARQUIVO INVALIDO"<<endl;
 }
@@ -32,7 +33,6 @@ void retract() {
 void verificaLinha() {
   int verifica = (int)buffer[cont];
   if (verifica==10) {
-    last = coluna;
     linha++;
     coluna=0;
   }
@@ -47,35 +47,36 @@ int main() {
   vector<string> reservadas;
   set<int> range;
   range.insert(9); range.insert(10);
-  for (i=0;i<94; i++) {
-    range.insert(r);
-    r++;
+  for (i=32;i<=126; i++) {
+    range.insert(i);
   }
   reservadas.push_back("ATEH"); reservadas.push_back("SE"); reservadas.push_back("BIT"); reservadas.push_back("DE");reservadas.push_back("ENQUANTO"); reservadas.push_back("ESCREVA"); reservadas.push_back("FIM"); reservadas.push_back("FUNCAO"); reservadas.push_back("INICIO"); reservadas.push_back("LEIA"); reservadas.push_back("NULO"); reservadas.push_back("PARA"); reservadas.push_back("INTEIRO"); reservadas.push_back("PARE"); reservadas.push_back("REAL"); reservadas.push_back("RECEBA"); reservadas.push_back("SE"); reservadas.push_back("SENAO"); reservadas.push_back("VAR"); reservadas.push_back("VET");
   vector< pair<int,string> > TOKENS;
   vector< pair<int,string> >:: iterator it; // 0/reservada, 1/id, 2/num
   string s;
   bool arqv_inv = false, ok = false;
-  while(!feof(fp)) {
-    fscanf(fp,"%c",&cy);
-    int tabela = (int)cy;
-    if (range.count(tabela) <= 0) {
-        arqv_inv = true;
-      }
-    else
-      buffer.push_back(cy);
+  char line[10000];
+  int oi =0;
+  while (fgets(line, 10000, stdin)) {
+      buffer+= line;
+  }
+
+  for (i=0; i<buffer.size(); i++) {
+      int tabela = (int)buffer[i];
+      if (range.count(tabela) <= 0) {
+          arqv_inv = true;
+          bug = 1;
+        }
   }
   if (buffer.size()== 0) {
       cout<<"OK"<<endl;
       ok= true;
   }
+  else if (buffer.size()>0 && arqv_inv == true)
+    fail();
   else {
-    if (arqv_inv== true) {
-      fail();
-    }
-    else {
-        buffer.resize(buffer.size()-1);
   while (cont != buffer.size()) {
+    //cout<<"linha colunas"<< linha <<" "<<coluna<<endl;
     if (state==-1) break;
     switch (state) {
       case 0: 
@@ -213,13 +214,20 @@ int main() {
            s.clear();
          }
          else if (buffer[cont]=='"'){
-            //abriu = true;
+            abriu=true;
             if (cont == buffer.size()-1) {
-              erro();
+              bug=1;
+              if (lixo !=1) {
+                  cout<< linha <<" " << coluna<<endl;
+                }
+              else{
+                   cout<< linha <<" " << coluna+1<<endl;
+                }
               s.clear();
+              }
+            else{
+              state = 8;
             }
-          else
-           state = 8;
          }
          else if (buffer[cont]=='&'){
            state = 0;
@@ -244,14 +252,15 @@ int main() {
          }
          else if (temp == 32 || temp == 10 || temp == 9){
               verificaLinha();
-              state= 6;  
+              //state= 6;  
             } 
          else {
             if(cont < buffer.size()) {
-              erro(); state=0;
+              erro(); state=0;   
             }
-            else
+            else{
               break;   
+            }
           }     
        }break;
 
@@ -270,8 +279,7 @@ int main() {
                     state = 1; s.push_back(buffer[cont]);
               }               
           } 
-        else if (buffer[cont]==' '|| aux==10 || aux==9){
-          verificaLinha();
+        else if (aux == 32 || aux==10 || aux==9){
           state = 6;
           for (i=0; i<reservadas.size(); i++) {
             if (reservadas[i].compare(s)==0) {
@@ -342,7 +350,7 @@ int main() {
       {
         getNextChar();
          if (buffer[cont]=='0' || buffer[cont]=='1' || buffer[cont]=='2' || buffer[cont]=='3' || buffer[cont]=='4' || buffer[cont]=='5' || buffer[cont]=='6' || buffer[cont]=='7' || buffer[cont]=='8' || buffer[cont]=='9') {
-           if(s.size()==3) {
+           if(s.size()==512) {
                   state=0;
                   s.clear();
                   retract();
@@ -367,7 +375,7 @@ int main() {
       }break;
       case 6: // tratando espaÃ§o, tab e quebra de linha
       {
-        //cout<<"estado 6"<<endl;
+        verificaLinha();
         getNextChar();
         int temp2 = int(buffer[cont]);
         if(temp2 ==32 || temp2 == 10 || temp2==9)
@@ -382,7 +390,7 @@ int main() {
       {
         getNextChar();
         if (buffer[cont]=='0' || buffer[cont]=='1' || buffer[cont]=='2' || buffer[cont]=='3' || buffer[cont]=='4' || buffer[cont]=='5' || buffer[cont]=='6' || buffer[cont]=='7' || buffer[cont]=='8' || buffer[cont]=='9') {
-          if(s.size()==3) {
+          if(s.size()==512) {
                   state=0;
                   s.clear();
                   retract();
@@ -393,51 +401,87 @@ int main() {
             }     
         }
         else  {
-            state=0; s.clear(); retract(); erro();
+        	if (isalpha(buffer[cont]) != 0){
+        		state=0; s.clear(); retract(); erro();
+        	}
+        	else {
+        		state=0; s.clear(); retract();
+        	}
+            
         }
       }break;
 
       case 8: //string
       {
-        int temp3 = int(buffer[cont]);
-        getNextChar();
+        getNextChar(); lixo =0;
+        //cout<<linha<<" "<<coluna<<endl;
+         int temp4 = int(buffer[cont]);
+        if (cont ==  buffer.size()-1) {
+          cout<<linha<<" "<<coluna-1<<endl;
+          s.clear(); para = true; bug = 1;
+        }
         if (buffer[cont]=='"') {
+        	if (enter== true){
+        		linha++; coluna=1;
+        		enter=false;
+        	}
+            fechou=true;
             TOKENS.push_back(make_pair(1,s));
+            s.clear();
             state=0;
           }
-        else {
-          if (s.size()==3) {
-            if (temp3==10) {
-              linha--; coluna= last; s.clear(); state=0; 
-              erro(); last=0; linha++; coluna=1;
-            }
-            else {
-              s.clear(); retract(); erro(); state=0;
-            }    
-          }
-          else {
-              if (cont < buffer.size()) {
-                  verificaLinha();
-                  s.push_back(buffer[cont]);
-                  state=8;
+        else { // nao fechou
+            if (s.size()==512) { // estourou
+              if (enter==true) { // teve enter?
+                  cout<< slinha <<" "<< scol<< endl;
+                  bug=1;
+                  linha++;
+                  coluna=1;
+                  state=0; s.clear();
+                  enter=false;
+                  lixo = 1;
               }
               else {
-                if (temp3==10) {
-                    linha--; coluna= last; s.clear(); state=0; 
-                    erro(); last=0; linha++; coluna=1;
-                  }
-                else {
-                   s.clear(); state = 0; erro();
-                  }        
-                }
+                  retract();
+                  erro();
+                  state=0; s.clear();
               }
+              
             }
+            else { // nao estourou
+                if (enter== true) {
+                    linha++; coluna=1; enter=false;
+                }
+                if (temp4==10) {
+                  s.push_back(buffer[cont]);
+                  enter= true;
+                  scol = coluna; slinha = linha;
+                  state=8;
+                }
+                else {
+                    s.push_back(buffer[cont]);
+                    state =8;  
+                }
+            }
+          }
         }break;
       }
     }
-    }
   } 
-  if (bug==0 && ok== false)
+  if (s.size() > 0 && para==false) {
+    bug=1;
+    if (error==1) {
+      if (coluna==0) {
+        cout<<slinha<<" "<<scol+1<<endl;
+      }
+      else {
+        cout<<slinha<<" "<<scol<<endl;
+      }
+    }
+    else
+      cout<<linha<<" "<< coluna+1<<endl;
+  }
+  if (bug==0 && ok==false)
     cout<<"OK"<<endl;
   //cout<<endl;
 }
